b34c37a5909ad1484775ae7d1fc2fa71919a0f34
Axel Cuevas <axelcoevas@gmail.com>
2021-07-30 17:06:11 -0500

    Añadiendo mas soluciones

--- attempts/example2-solution1.js
+++ attempts/example2-solution1.js
@@ -1,8 +1,8 @@
 /*RETO 2*/
 
-/*Escribir una función chunk que recibe 
-un arreglo y un número entero size. 
-La función debe dividir el arreglo en múltiples 
+/*Escribir una función chunk que recibe
+un arreglo y un número entero size.
+La función debe dividir el arreglo en múltiples
 arreglos del tamaño determinado por size.*/
 
 function chunk(array, size) {
@@ -27,3 +27,22 @@ var data = [1, 2, 3, 4, 5, 6, 7, 8];
 console.log("Test 1:", chunk(data, 1)); // [[1], [2], [3], [4], [5], [6], [7], [8]]
 console.log("Test 2:", chunk(data, 2)); // [[1, 2], [3, 4], [5, 6], [7, 8]]
 console.log("Test 3:", chunk(data, 3)); // [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+
+//Solución Axel
+
+function chunk(array, size) {
+  let newArray = [];
+  let mini = [];
+  for (let i = 0; i < array.length; i++) {
+    mini.push(array[i]);
+    if ((i + 1) % size == 0 || (i + 1) == array.length) {
+      newArray.push(mini);
+      mini = [];
+    }
+  }
+
+  return newArray;
+}
+
+console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
--- attempts/example3-solution1.js
+++ attempts/example3-solution1.js
@@ -1,10 +1,10 @@
 /*Reto 3*/
 
 /*
-Escribir una función frequency que recibe un string como argumento. 
-Esta función debe contar la frecuencia o el número de veces que se 
-repite cada carácter. El resultado debe mostrarse en un objeto donde 
-las propiedades sean los caracteres, y los valores sean la frecuencia. 
+Escribir una función frequency que recibe un string como argumento.
+Esta función debe contar la frecuencia o el número de veces que se
+repite cada carácter. El resultado debe mostrarse en un objeto donde
+las propiedades sean los caracteres, y los valores sean la frecuencia.
 Los resultados deben ordenarse ascendentemente por los caracteres y no
 la frecuencia.
 */
@@ -19,9 +19,9 @@ function frequency(str) {
       aux == 0 || strArray[aux] != strArray[aux - 1]
         ? { ...finalObj, [strArray[aux]]: 1 }
         : {
-            ...finalObj,
-            [strArray[aux - 1]]: finalObj[`${strArray[aux - 1]}`] + 1,
-          };
+          ...finalObj,
+          [strArray[aux - 1]]: finalObj[`${strArray[aux - 1]}`] + 1,
+        };
   }
   return finalObj;
 }
@@ -32,3 +32,19 @@ console.log("Test 2:", frequency("www.bedu.org"));
 // {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
 console.log("Test 3:", frequency("john.doe@domain.com"));
 // {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
+
+
+//Solución de Axel
+
+function frecuency2(string) {
+  for (let i = 0; i < string.length; i++) {
+    let char = string.charAt(i);
+    if (frecuency[char]) {
+      frecuency[char]++;
+    } else {
+      frecuency[char] = 1;
+    }
+  }
+
+  return frecuency;
+}
