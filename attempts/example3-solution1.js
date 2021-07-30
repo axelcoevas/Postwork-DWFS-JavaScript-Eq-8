0e5243b5f17ed6c6fe977a9700d1c6e0811f1a2a
Antonio Labra <antonio_labra@hotmail.com>
2021-07-30 02:41:42 -0500

    se subio el reto 3 generando un array del string y ordenandolo usando la funcion sort, despues, se valido si el valor actual era diferente del anterior y en caso de ser correcto se generaba una nueva key en nuestro objeto, si ya estaba repetida, buscabamos el valor de la key y le sumabamos uno. Para tener la copia del objeto se utilizo spread operator y para asignar y validar el objeto se utilizo un operador ternario

---
+++ attempts/example3-solution1.js
@@ -0,0 +1,34 @@
+/*Reto 3*/
+
+/*
+Escribir una función frequency que recibe un string como argumento. 
+Esta función debe contar la frecuencia o el número de veces que se 
+repite cada carácter. El resultado debe mostrarse en un objeto donde 
+las propiedades sean los caracteres, y los valores sean la frecuencia. 
+Los resultados deben ordenarse ascendentemente por los caracteres y no
+la frecuencia.
+*/
+
+function frequency(str) {
+  let strArray = str.split("").sort();
+  let aux = -1;
+  let finalObj = {};
+  for (let i = 0; i < strArray.length; i++) {
+    aux++;
+    finalObj =
+      aux == 0 || strArray[aux] != strArray[aux - 1]
+        ? { ...finalObj, [strArray[aux]]: 1 }
+        : {
+            ...finalObj,
+            [strArray[aux - 1]]: finalObj[`${strArray[aux - 1]}`] + 1,
+          };
+  }
+  return finalObj;
+}
+
+console.log("Test 1:", frequency("cccbbbaaa"));
+// {a: 3, b: 3, c: 3}
+console.log("Test 2:", frequency("www.bedu.org"));
+// {.: 2, b: 1, d: 1, e: 1, g: 1, o: 1, r: 1, u: 1, w: 3}
+console.log("Test 3:", frequency("john.doe@domain.com"));
+// {.: 2, @: 1, a: 1, c: 1, d: 2, e: 1, h: 1, i: 1, j: 1, m: 2, n: 2, o: 4}
