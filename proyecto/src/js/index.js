36d78ff8b21beb4b3f67e36e74a22774d1c47af5
Axel Cuevas <axelcoevas@gmail.com>
2021-08-17 23:45:33 -0500

    Terminando back para detail

--- proyecto/src/js/index.js
+++ proyecto/src/js/index.js
@@ -34,11 +34,15 @@ function printReceipeDetail(data) {
   const instructions = document.getElementById('instructions');
   const strYoutube = document.getElementById('strYoutube');
   const tags = document.getElementById('tags');
+  const carousel = document.getElementById('carousel');
+
+  clearContainers([instructions, recommendRow, tags, carousel]);
 
   strMeal.innerHTML = meal.strMeal;
   strMealThumb.src = meal.strMealThumb;
 
-  const ingredients = getIngredients(meal);
+  printIngredients(meal);
+  carousel.firstChild.className = 'carousel-item active';
 
   meal.strInstructions.split('.').forEach(e => {
     const li = document.createElement('li');
@@ -49,31 +53,80 @@ function printReceipeDetail(data) {
   strYoutube.src = meal.strYoutube.replace('watch?v=', 'embed/');
   strYoutube.title = meal.strMeal;
 
-  meal.strTags.split(',').forEach(e => {
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.className = 'btn btn-outline-dark';
-    button.innerHTML = '#' + e;
-    tags.appendChild(button);
-  });
+  if (meal.strTags != null) {
+    tags.innerHTML = "<h2>Tags</h2>";
+    meal.strTags.split(",").forEach((e) => {
+      const button = document.createElement("button");
+      button.type = "button";
+      button.className = "btn btn-outline-dark";
+      button.innerHTML = "#" + e;
+      tags.appendChild(button);
+    });
+  }
 
   getRecommended(meal.strCategory);
-
-  console.log(meal);
 }
 
-function getIngredients(data) {
+function clearContainers(containerList) {
+  containerList.forEach((container) => {
+    while (container.firstChild) {
+      container.removeChild(container.lastChild);
+    }
+  });
+
+  scrollUp();
+};
+
+function scrollUp() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+function printIngredients(data) {
   const ings = [];
   for (let i = 1; i <= 20; i++) {
-    if (data['strIngredient' + i] !== null) {
+    const ingredient = data['strIngredient' + i];
+    const measure = data['strMeasure' + i];
+    if (ingredient) {
       const ing = {
-        "ingredient": data['strIngredient' + i].replaceAll(' ', '_'),
-        "measure": data['strMeasure' + i]
+        "ingredient": ingredient,
+        "measure": measure
       };
       ings.push(ing);
     }
   }
-  return ings;
+
+  ings.forEach(e => {
+    printIngredient(e);
+  });
+}
+
+function printIngredient(ing) {
+  const carousel = document.getElementById('carousel');
+
+  const carouselItem = document.createElement('div');
+  carouselItem.className = 'carousel-item';
+  carouselItem.setAttribute("data-bs-interval", "3500");
+
+  const img = document.createElement('img');
+  img.src = `https://www.themealdb.com/images/ingredients/${ing.ingredient}.png`;
+  img.className = 'd-block w-100';
+  img.alt = ing.ingredient;
+  carouselItem.appendChild(img);
+
+  const carouselCaption = document.createElement('div');
+  carouselCaption.className = 'carousel-caption d-none d-md-block';
+
+  const h5 = document.createElement('h5');
+  h5.innerHTML = ing.ingredient;
+  carouselCaption.appendChild(h5);
+
+  const p = document.createElement('p');
+  p.innerHTML = ing.measure;
+  carouselCaption.appendChild(p);
+
+  carouselItem.appendChild(carouselCaption);
+
+  carousel.appendChild(carouselItem);
 }
 
 async function getRecommended(area) {
@@ -109,6 +162,14 @@ function printRecommended(data) {
     //agregar funcionamiento de onclick
     a.className = 'btn btn-dark';
     a.innerText = 'Learn more';
+    a.addEventListener("click", () => {
+      clearContainers([
+        recommendRow,
+        tags,
+        document.querySelector(".carousel-inner"),
+      ]);
+      getMeal(e.idMeal);
+    });
 
     cardBody.appendChild(h5);
     cardBody.appendChild(a);
